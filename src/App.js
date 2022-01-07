8635e8ac8f97efacebfda82d66a6c645a7540d1e
Truong Anh <truonganh20062001@gmail.com>
2022-01-07 19:20:48 +0700

    render element

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 function App() {
+  const name = "Hau";
+  const age = 18;
+  const isMale = true;
+  const student = {
+    name: "Ez FE",
+  };
+
+  const colorList = ["green", "red", "blue"];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Xin chao {name} - {age} - {isMale ? "Male" : "Female"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vip rorsd
-        </a>
+        {isMale ? <p>Male</p> : <p>Female</p>}
+
+        {isMale && <p>Male</p>}
+        {!isMale && <p>Female</p>}
+
+        {isMale && (
+          <div>
+            <p>Male 1</p>
+            <p>Male 2</p>
+            <p>Male 3</p>
+          </div>
+        )}
+
+        {isMale && (
+          <React.Fragment>
+            <p>Male 1</p>
+            <p>Male 2</p>
+            <p>Male 3</p>
+          </React.Fragment>
+        )}
+
+        {isMale && (
+          <>
+            <p>Male 1</p>
+            <p>Male 2</p>
+            <p>Male 3</p>
+          </>
+        )}
+        {/* Render object */}
+        <p>{student.name}</p>
+
+        {/* Render mãng */}
+        <ul>
+          {colorList.map((color) => (
+            <li style={{ color }}>{color}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
